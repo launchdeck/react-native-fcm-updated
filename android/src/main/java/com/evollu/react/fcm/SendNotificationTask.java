f6af05138847311716d1a56f3f505d4f99855f95
Pham Hoang Phat <phat@launchdeck.org>
2020-01-09 20:59:25 +0700

    Updated source file to AndroidX

--- android/src/main/java/com/evollu/react/fcm/SendNotificationTask.java
+++ android/src/main/java/com/evollu/react/fcm/SendNotificationTask.java
@@ -16,10 +16,11 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
